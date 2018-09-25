be00d9edd6d46a78d2e7cae4f0d333ff23bfeb5d
yerkewka <yerkewka@mail.ru>
2018-09-26 01:22:22 +0600

    registration and login services done

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,6 @@
 module.exports = {
   mongoURI:
-    'mongodb://yerkewka:yerkewka1995@ds111993.mlab.com:11993/devconnector-react'
+    // 'mongodb://yerkewka:yerkewka1995@ds111993.mlab.com:11993/devconnector-react'
+    'mongodb://yerkewka:yerkewka1995@ds113873.mlab.com:13873/devconnector-react',
+  secretOrKey: 'secret'
 };
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,6 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
+// @route       GET api/profile/test
+// @description Tests profile route
+// @access      Public route
 router.get('/test', (req, res) => {
   res.json({ msg: 'Profile works' });
 });
